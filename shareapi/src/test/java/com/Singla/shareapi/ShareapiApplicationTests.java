08494a7dab7f08a7a79dddc3401d414c2b26558b
Himanshu singla <singlah2505@gmail.com>
2026-01-27 23:55:17 +0530

    springboot project initiated

---
+++ shareapi/src/test/java/com/Singla/shareapi/ShareapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Singla.shareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
